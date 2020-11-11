e0b32bd7bf2b09da06ad47591b611383844f86aa
Salavei Dzmitry <salavei.dv@gmail.com>
2020-11-12 01:25:01 +0300

    feat: implements follow/unfollow on user page

--- app/routes.py
+++ app/routes.py
@@ -96,3 +96,39 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    """
+    implementations follow API
+    """
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {user} not found.')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash(f'You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f'You are following {username}')
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    """
+    docstring
+    """
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
