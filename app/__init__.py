8a1eacfb5f2eb38e2936a822d79f249cbedd222a
Salavei Dzmitry <salavei.dv@gmail.com>
2020-11-14 02:14:23 +0300

    feat: add bootstrap framework to project

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import os
 import logging
 
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 from app import routes, models, errors
