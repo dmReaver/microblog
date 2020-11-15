0ed628c0e5924fdfdef8c80f537c5170d84c120f
Salavei Dzmitry <salavei.dv@gmail.com>
2020-11-16 00:30:38 +0300

    feat: split app to blueprints

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from app import db
 from app import login
-from app import app
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
 import jwt
+from flask import current_app
 
 
 followers = db.Table('followers',
